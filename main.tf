e8f1bb8b1be315c608e3ad654ae6c0372cb3bad3
prasanthsompalli12 <92778941+prasanthsompalli12@users.noreply.github.com>
2021-11-02 11:18:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-resource "azurerm_resource_group" "vmrg" {
+resource "azurerm_resource_group" "prasanthrg" {
   name     = var.rgname
   location = var.location
 }
 
-resource "random_id" "vm-sa" {
+resource "random_id" "prasanth-sa" {
   keepers = {
     vm_hostname = var.vm_hostname
   }
@@ -12,8 +12,8 @@ resource "random_id" "vm-sa" {
 
 resource "azurerm_storage_account" "storage" {
   name                     = "bootdsk${lower(random_id.vm-sa.hex)}"
-  resource_group_name      = azurerm_resource_group.vmrg.name
-  location                 = azurerm_resource_group.vmrg.location
+  resource_group_name      = azurerm_resource_group.prasanthrg.name
+  location                 = azurerm_resource_group.prasanthrg.location
   account_tier             = element(split("_", var.account_tier), 0)
   account_replication_type = element(split("_", var.account_tier), 1)
   #tags = matchkeys(var.tags,"2")
